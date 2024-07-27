64cd82d31b130cf739af4d79066c4232e0e3384e
Enrique Alpízar <ealpizr@gmail.com>
2024-07-27 15:47:10 -0600

    Add `ICommand` interface and `PlayCardCommand` class

---
+++ core/Commands/PlayCardCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace godotcards.core.Commands
+{
+    public class PlayCardCommand : ICommand
+    {
+        private PlayerBase player;
+        private Card card;
+
+        public PlayCardCommand(PlayerBase player, Card card) { }
+
+        public void Execute()
+        {
+            if (!player.Hand.Cards.Contains(card))
+            {
+                throw new Exception("Illegal move, player does not have this card in their hand.");
+            }
+
+            player.Hand.Cards.Remove(card);
+            player.PlayHand.Cards.Add(card);
+            card.Reparent(player.PlayingFieldContainer);
+        }
+
+        public void Undo()
+        {
+            if (!player.PlayHand.Cards.Contains(card))
+            {
+                throw new Exception("Illegal move, player does not have this card in their play hand.");
+            }
+
+            player.PlayHand.Cards.Remove(card);
+            player.Hand.Cards.Add(card);
+            card.Reparent(player.Hand);
+        }
+    }
+}
